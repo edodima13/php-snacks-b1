08c71a0883cc41054f9bd265b23f0ed73dc000c2
edodima13 <edodima13@libero.it>
2022-03-30 17:48:06 +0200

    avanzamento snack-2

---
+++ snack-2/index.php
@@ -0,0 +1,13 @@
+<?php
+//Passare come parametri GET name, mail e age
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $age = $_GET['age'];
+//verificare che name sia più lungo di 3 caratteri, 
+//che mail contenga un punto e una chiocciola ,
+//che age sia un numero.
+//stampare “Accesso riuscito”, altrimenti “Accessonegato”
+?>
+<?php
+    
+?>
