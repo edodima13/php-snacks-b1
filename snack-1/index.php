9bab54fccf097b61281c4f443878ee4300638253
edodima13 <edodima13@libero.it>
2022-03-30 17:03:51 +0200

    avanzamento snack-1

---
+++ snack-1/index.php
@@ -0,0 +1,38 @@
+<?php
+//creare un array contenenti delle partite di basket
+$partite = [
+    [
+    'casa' => 'Basket Milano',
+    'opsite' => 'Basket Roma',
+    'punti_casa' => 45,
+    'punti_opsite' => 21,
+    ],
+    [
+    'casa' => 'Basket Catania',
+    'opsite' => 'Basket Palermo',
+    'punti_casa' => 45,
+    'punti_opsite' => 21,
+    ],
+    [
+    'casa' => 'Basket ',
+    'opsite' => 'Basket Roma',
+    'punti_casa' => 45,
+    'punti_opsite' => 21,
+    ]
+    ];
+?>
+<?php
+//stampare a schermo con un ciclo for nel seguente schema: Olimpia Milano- Cantù | 55-60
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
