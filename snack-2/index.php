b1b4aec95cdac465715ee482e6f46bb4139bae8d
edodima13 <edodima13@libero.it>
2022-03-30 17:54:20 +0200

    avanzamento snack-2

--- snack-2/index.php
+++ snack-2/index.php
@@ -3,7 +3,10 @@
     $name = $_GET['name'];
     $mail = $_GET['mail'];
     $age = $_GET['age'];
-//verificare che name sia più lungo di 3 caratteri, 
+//verificare che name sia più lungo di 3 caratteri,
+    if(strlen($name) > 3){
+        echo 'il nome è valido';
+    };
 //che mail contenga un punto e una chiocciola ,
 //che age sia un numero.
 //stampare “Accesso riuscito”, altrimenti “Accessonegato”
